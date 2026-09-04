cefb55e857fc0814d33ce0e0b15c24e537dfb3fc
lenaldosouza <souvinilenaldo@gmail.com>
2026-09-04 10:31:26 -0300

    refactor: criando construtores para as classes.

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/Carteira.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/Carteira.cs
@@ -3,7 +3,20 @@
     public class Carteira
     {
         public int NumeroCarteira { get; set; }
-        public string NomeCarteira { get; set; }
+        public string NomeCarteira { get; set; } = string.Empty;
         public decimal ApetiteCarteira { get; set; }
+
+        public Carteira(int numeroCarteira, string nomeCarteira, decimal apetiteCarteira)
+        {
+            NumeroCarteira = numeroCarteira;
+            NomeCarteira = nomeCarteira;
+            ApetiteCarteira = apetiteCarteira;
+
+            if (ApetiteCarteira < NumeroCarteira)
+            {
+                throw new ArgumentException("O valor do apetite deve ser maior ao valor da carteira");
+            }
+        }
+        Carteira carteiraTeste = new Carteira(0000,"Teste",0001);
     }
 }
