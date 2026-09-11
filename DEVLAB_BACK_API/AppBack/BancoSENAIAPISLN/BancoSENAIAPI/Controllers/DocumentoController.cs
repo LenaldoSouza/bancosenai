d53a9f2a1bec167b44e462962c20dc1479e7c5d1
Lilly <265367972+LenaldoSouza@users.noreply.github.com>
2026-09-11 08:26:01 -0300

    feat(documentos) criacao de controller e model de documentos

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BancoSENAIAPI.Controllers
+{
+    [ApiControler]
+    [Route("api/v1/[controller]")]
+    public class DocumentoController : Controllers
+    {
+
+    }
+}
