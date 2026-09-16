db706e8d72a17d8957a0edd43b3f55385cc07eb9
Lilly <265367972+LenaldoSouza@users.noreply.github.com>
2026-09-16 08:07:44 -0300

    Update DocumentoController.cs

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -26,6 +26,7 @@ namespace BancoSENAIAPI.Controllers
                 return BadRequest("Nenhum arquivo foi enviado.");
             }
 
+            // R06F - Limite de tamanho: 2 MB
             const long limiteTamanho = 2 * 1024 * 1024;
 
             if (arquivo.Length > limiteTamanho)
@@ -35,6 +36,7 @@ namespace BancoSENAIAPI.Controllers
                 );
             }
 
+            // R06G - Extensões permitidas
             string extensao = Path.GetExtension(arquivo.FileName)
                 .ToLowerInvariant();
 
@@ -45,6 +47,56 @@ namespace BancoSENAIAPI.Controllers
                 ".png"
             };
 
+            if (!extensoesPermitidas.Contains(extensao))
+            {
+                return BadRequest(
+                    "Extensão de arquivo não permitida. " +
+                    "Use apenas arquivos .pdf, .jpg ou .png."
+                );
+            }
+
+            string pastaCliente = Path.Combine(
+                _caminhoRaiz,
+                codigoCliente.ToString()
+            );
+
+            if (!Directory.Exists(pastaCliente))
+            {
+                Directory.CreateDirectory(pastaCliente);
+            }
+
+            string nomeOriginal =
+                Path.GetFileNameWithoutExtension(arquivo.FileName);
+
+            string novoNome =
+                $"{codigoCliente}_{nomeOriginal}_{Guid.NewGuid()}{extensao}";
+
+            string caminhoFinal =
+                Path.Combine(pastaCliente, novoNome);
+
+            using (var stream = new FileStream(
+                caminhoFinal,
+                FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
+
+            var documentoMetadados = new Models.DocumentoMetadado
+            {
+                Id = _nextId++,
+                Nome = nomeOriginal,
+                Extensao = extensao,
+                Caminho = caminhoFinal,
+                CodigoCliente = codigoCliente,
+            };
+
+            _documentosMetadados.Add(documentoMetadados);
+
+            return Ok(new
+            {
+                mensagem = "Documento anexado com sucesso",
+                arquivoSalvo = novoNome
+            });
         }
     }
 }
